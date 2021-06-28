dfb865ea0273a498c913b19553da4bcbae0e5ecd
yusuf6002 <86309730+yusuf6002@users.noreply.github.com>
2021-06-28 20:44:12 +0530

    Add files via upload

---
+++ Trex_Stage_0-main/Trex_Stage_0-main/sketch.js
@@ -0,0 +1,42 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadImage("trex_collided.png");
+  groundImage = loadImage("ground2.png")
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  
+  trex.scale = 0.5;
+  trex.x = 50
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+}
+
+function draw() {
+  background(220);
+  
+  ground.velocityX = -2
+  console.log(ground.x)
+  
+  if (ground.x<0){
+    ground.x = ground.width/2;
+  }
+  
+  if(keyDown("space")) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  trex.collide(ground);
+  drawSprites();
+}
